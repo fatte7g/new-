85075b9bed87ecd09af63d30837c74ca2f0709e6
fatte7g <puriphatte@gmail.com>
2025-07-17 10:38:23 +0545

    change

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navb from './Navb'
+import Login from './Login'
+
+const App = () => {
+  return (
+    <div>
+<Navb/>
+<Login/>
+    </div>
+  )
+}
+
+export default App
